714916d2227c834a2fa4257eb6584891202b05d3
streafy <streafyhs@gmail.com>
2025-07-02 16:38:04 +0700

    feat: add some uikit components

--- app/src/main/kotlin/streafy/leasingshift2025/MainActivity.kt
+++ app/src/main/kotlin/streafy/leasingshift2025/MainActivity.kt
@@ -4,44 +4,52 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import streafy.leasingshift2025.ui.theme.Leasingshift2025Theme
+import androidx.compose.ui.unit.dp
+import streafy.leasingshift2025.uikit.components.Button
+import streafy.leasingshift2025.uikit.components.ButtonVariant
+import streafy.leasingshift2025.uikit.components.Input
+import streafy.leasingshift2025.uikit.theme.LeasingTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContent {
-            Leasingshift2025Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            LeasingTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    containerColor = LeasingTheme.colors.bgPrimary
+                ) { innerPadding ->
+                    Column(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .padding(0.dp)
+                            .fillMaxSize(),
+                        verticalArrangement = Arrangement.Top,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Input(label = "Поиск", placeholder = "Поиск")
+                        Input(label = "Даты аренды", placeholder = "10 - 24 июля 2025 (14 дней)")
+                        Button(
+                            onClick = {},
+                            modifier = Modifier.fillMaxWidth(0.8f),
+                            variant = ButtonVariant.PrimaryDark
+                        ) {
+                            Text(text = "Фильтры")
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Leasingshift2025Theme {
-        Greeting("Android")
     }
 }
